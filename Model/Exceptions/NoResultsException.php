005a908c359b77022a5368f51cfad1c7539f13fe
blackice999 <adam596341@yahoo.com>
2017-05-21 20:03:57 +0300

    Create structure for database operations

---
+++ Model/Exceptions/NoResultsException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adam
+ * Date: 21.05.2017
+ * Time: 19:13
+ */
+
+namespace Model;
+
+
+class NoResultsException extends \Exception
+{
+
+}
---
+++ Model/Exceptions/QueryException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adam
+ * Date: 21.05.2017
+ * Time: 19:12
+ */
+
+namespace Model;
+
+
+class QueryException extends \Exception
+{
+
+}
