1145fd8d2e636a06d987a27ff1defecff29f4aa1
blackice999 <adam596341@yahoo.com>
2017-05-21 18:56:36 +0300

    Generate first page structure

--- index.php
+++ index.php
@@ -1,7 +1,95 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+    <link rel="stylesheet" type="text/css" href="css/foundation.css">
+</head>
+<body>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Adam
- * Date: 21.05.2017
- * Time: 17:55
- */
+
+include "menu.php";
+
+//if(!isset($_SESSION['userId'])) {
+?>
+<div class="row">
+    <div class="large-6 columns float-right">
+        <h2> Login</h2>
+        <form method="post" action="login.php">
+            <label>
+                Alias
+                <input type="text" name="name">
+            </label>
+            <label>
+                Password
+                <input type="password" name="password">
+            </label>
+
+            <input type="submit" class="button" value="Login" name="login">
+        </form>
+    </div>
+</div>
+
+<div class="row">
+    <div class="large-6 columns">
+        <h2> Register</h2>
+        <form method="post" action="register.php">
+            <label>
+                Name
+                <input type="text" name="name">
+            </label>
+
+            <label>
+                Alias
+                <input type="text" name="alias">
+            </label>
+
+            <label>
+                Email
+                <input type="text" name="email">
+            </label>
+
+            <label>
+                Telephone number
+                <input type="text" name="telephone">
+            </label>
+
+            <label>
+                Date of birth
+                <input type="text" name="date_of_birth">
+            </label>
+
+            <label>
+                Address
+                <input type="text" name="address">
+            </label>
+
+            <label>
+                Gender
+                <select name="gender">
+                    <option value="male">Male</option>
+                    <option value="male">Female</option>
+                </select>
+            </label>
+
+            <label>
+                Password
+                <input type="password" name="password">
+            </label>
+
+            <label>
+                Confirm password
+                <input type="password" name="confirm_password">
+            </label>
+
+            <input type="submit" class="button" value="Register" name="register">
+        </form>
+
+    </div>
+</div>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="large-10 columns">
+        <ul class="menu">
+            <li><a href="index.php">Homepage</a></li>
+        </ul>
+    </div>
+</div>
