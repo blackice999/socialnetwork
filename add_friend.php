2021328343eba1618d426dd6cc9555c631b36876
blackice999 <adam596341@yahoo.com>
2017-05-24 14:58:48 +0300

    Add friend for the logged in user

---
+++ add_friend.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adam
+ * Date: 24.05.2017
+ * Time: 14:41
+ */
+
+include "config.php";
+session_start();
+$errors = [];
+if (isset($_POST['add_friend'])) {
+    $email = \Utils\StringUtils::sanitizeString($_POST['email']);
+
+    if (empty($email)) {
+        $errors[] = "Email is not valid";
+    }
+
+    if (!\Model\UtilizatorEmailModel::loadByEmail($email)) {
+        $errors[] = "User does not exist";
+    }
+    $friendModel = \Model\UtilizatorEmailModel::loadByEmail($email);
+
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo $error;
+        }
+    } else {
+        \Model\PrieteniModel::create($_SESSION['userId'], $friendModel['id_utilizator']);
+        header("Location: edit_user.php");
+    }
+}
